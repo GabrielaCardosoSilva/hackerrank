a4b2cb5b11dabcf5d7c188efe1c74a56baf004df
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-09-10 19:14:38 -0300

    Update Day3_IntroToCondionalStatements.py

--- Day3_IntroToCondionalStatements.py
+++ Day3_IntroToCondionalStatements.py
@@ -16,7 +16,7 @@ import sys
 
 if __name__ == '__main__':
     N = int(input().strip())
-    if (N%2 == 1) or ((N%2 == 0) and 6 <+ N and N <= 20):
+    if (N%2 == 1) or ((N%2 == 0) and 6 <= N and N <= 20):
         print("Weird")
     else:
         print("Not Weird")
