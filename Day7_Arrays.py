6505d12c4b8911114349250772099fa870f54256
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:27:14 -0300

    Update Day7_Arrays.py

--- Day7_Arrays.py
+++ Day7_Arrays.py
@@ -1,9 +1,16 @@
+''''
+Task
+Given an array, A, of N integers, print A's elements in reverse order as a single line of space-separated numbers.
 
-import math
-import os
-import random
-import re
-import sys
+Example
+A = [1, 2, 3, 4]
+Print 4 3 2 1. Each integer is separated by one space.
+
+Input Format
+
+The first line contains an integer, N (the size of our array).
+The second line contains N space-separated integers that describe array A's elements.
+'''
 
 def reverse (arr):
         newArr = arr[::-1]
