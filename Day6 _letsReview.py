e94f4d0f7baa8af5262fc17627be4771998e35c2
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:41:11 -0300

    Update Day6 _letsReview.py

--- Day6 _letsReview.py
+++ Day6 _letsReview.py
@@ -1,3 +1,10 @@
+'''
+Task
+Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters as 2 space-separated strings 
+on a single line (see the Sample below for more detail).
+Note: 0 is considered to be an even index.
+'''
+
 n = int(input())
 
 for c in range (n):
