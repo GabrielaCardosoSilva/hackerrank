bd9dd9588516d05ccbed17f71cb1f3b0a542331c
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:43:08 -0300

    Update Day3_IntroToCondionalStatements.py

--- Day3_IntroToCondionalStatements.py
+++ Day3_IntroToCondionalStatements.py
@@ -6,7 +6,8 @@ Given an integer, , perform the following conditional actions:
 * If  is even and in the inclusive range of  to , print Weird
 * If  is even and greater than , print Not Weird
 
-Complete the stub code provided in your editor to print whether or not  is weird.'''
+Complete the stub code provided in your editor to print whether or not  is weird.
+'''
 
 if __name__ == '__main__':
     N = int(input().strip())
