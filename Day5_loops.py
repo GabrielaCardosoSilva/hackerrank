96c7133a5344c48d2acfb826c7902b2e44796417
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:38:47 -0300

    Update Day5_loops.py

--- Day5_loops.py
+++ Day5_loops.py
@@ -4,7 +4,6 @@ Given an integer, n, print its first  multiples. Each multiple n x i
 (where 1 <= n <= 10) should be printed on a new line in the form: n x i = result.
 '''
 
-
 if __name__ == '__main__':
     n = int(input().strip())
     for i in range (1, 11):
