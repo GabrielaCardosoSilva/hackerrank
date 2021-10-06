4ad2daac9bd1f190ec60a27a8fca8492c9a0613b
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:33:14 -0300

    Update Day1_DataTypes.py

--- Day1_DataTypes.py
+++ Day1_DataTypes.py
@@ -7,6 +7,7 @@ Use the  operator to perform the following operations:
 Print the sum of  plus your int variable on a new line.
 Print the sum of  plus your double variable to a scale of one decimal place on a new line.
 Concatenate  with the string you read as input and print the result on a new line.'''
+
 i = 4
 d = 4.0
 s = 'HackerRank '
