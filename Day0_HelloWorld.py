8d7a29a3a165220ed00fe4afbb977acd6fba4989
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:42:03 -0300

    Update Day0_HelloWorld.py

--- Day0_HelloWorld.py
+++ Day0_HelloWorld.py
@@ -1,7 +1,10 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-# Print a string literal saying "Hello, World." to stdout.
-# TODO: Write a line of code that prints the contents of input_string to stdout.
-
+'''
+Task
+Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+Print a string literal saying "Hello, World." to stdout.
+TO-DO: Write a line of code that prints the contents of input_string to stdout.
+'''
+  
 input_string = input()
 print('Hello, World.')
 print(input_string)
