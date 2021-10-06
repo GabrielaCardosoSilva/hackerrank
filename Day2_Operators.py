2b87cb19525feb557c4e267a5e8487e53c0c8750
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:35:09 -0300

    Update Day2_Operators.py

--- Day2_Operators.py
+++ Day2_Operators.py
@@ -1,19 +1,8 @@
 '''Task
 Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
 and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's
-total cost. Round the result to the nearest integer.'''
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the 'solve' function below.
-# The function accepts following parameters:
-#  1. DOUBLE meal_cost
-#  2. INTEGER tip_percent
-#  3. INTEGER tax_percent
+total cost. Round the result to the nearest integer.
+'''
 
 def solve(meal_cost, tip_percent, tax_percent):
     print(round(((tip_percent/100) * meal_cost) + ((tax_percent/100) * meal_cost) + meal_cost))
