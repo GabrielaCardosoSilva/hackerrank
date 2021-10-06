b6a75ca7f196cb10a335417b5d3fe5c82c4aa77c
Gabriela Cardoso <gabrielacardososilva9@gmail.com>
2021-10-06 10:33:31 -0300

    Update Day1_DataTypes.py

--- Day1_DataTypes.py
+++ Day1_DataTypes.py
@@ -6,7 +6,8 @@ Read  lines of input from stdin (according to the sequence given in the Input Fo
 Use the  operator to perform the following operations:
 Print the sum of  plus your int variable on a new line.
 Print the sum of  plus your double variable to a scale of one decimal place on a new line.
-Concatenate  with the string you read as input and print the result on a new line.'''
+Concatenate  with the string you read as input and print the result on a new line.
+'''
 
 i = 4
 d = 4.0
